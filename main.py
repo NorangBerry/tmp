df02e8212a0af1411d3829b281080d9021188fd0
NorangBerry <hlnsud12@naver.com>
2021-11-17 00:15:50 +0900

    add trainer

--- main.py
+++ main.py
@@ -3,69 +3,70 @@ from smile.data_generator import DataGenerator
 from smile.opensmile_maker import CREMASmileMaker, SmileMaker
 from attack.attacker import Attacker, FeatureAttacker, FixedNoiseAttacker, RandomAttacker
 from utils.setting import DATASET_LIST, DATASET_PATH, ROOT_PATH
-from train.train import Trainer
+from train.train import CremaTrainer, IemocapTrainer, Trainer
 import os
 
 for dataset in DATASET_LIST:
-	generator = DataGenerator("CREMA-D")
-	generator.generate_from_one_wav()
-	# remove unused data
-	# TODO
-	# train
+    generator = DataGenerator("CREMA-D")
+    generator.generate_from_one_wav()
+    # remove unused data
+    # TODO
+    # train
+    CremaTrainer().train()
+    IemocapTrainer().train()
+    # test
+    # logs
+    
+    
+    exit(0)
+    json_dict = {}
+    # log = open('noise_attack_log.txt', 'a')
+    features = ["upleveltime","minPos","skewness","quartile","amean","stddev","kurtosis","iqr","linregc","F0finEnv","linregerrQ","F0final","numOnsets"]
+    # fixed_noises = ["noise-sound-bible-0039.wav","noise-sound-bible-0039.wav"]
+    fixed_noises = set()
+    for feature in features:
+        attacker = FeatureAttacker(os.path.join(ROOT_PATH,f"{feature}_attack"))
+        accuracy = attacker.test_attack(feature)
+        
+        attacker.noise_maker.remove_generated_files()
 
-	# test
-	# logs
-	
-	
-	exit(0)
-	json_dict = {}
-	# log = open('noise_attack_log.txt', 'a')
-	features = ["upleveltime","minPos","skewness","quartile","amean","stddev","kurtosis","iqr","linregc","F0finEnv","linregerrQ","F0final","numOnsets"]
-	# fixed_noises = ["noise-sound-bible-0039.wav","noise-sound-bible-0039.wav"]
-	fixed_noises = set()
-	for feature in features:
-		attacker = FeatureAttacker(os.path.join(ROOT_PATH,f"{feature}_attack"))
-		accuracy = attacker.test_attack(feature)
-		
-		attacker.noise_maker.remove_generated_files()
+        result_str = f"{feature} attack accuracy: {accuracy:.2f}\n"
+        print(result_str)
+        # log.write(result_str)
 
-		result_str = f"{feature} attack accuracy: {accuracy:.2f}\n"
-		print(result_str)
-		# log.write(result_str)
+        most_used_noise = attacker.get_most_used_noise(3)
+        fixed_noises.update(most_used_noise)
+        
+        json_dict[feature] = {
+            "accuracy":f"{accuracy:.2f}%",
+            "most_used_noise":most_used_noise
+        }
 
-		most_used_noise = attacker.get_most_used_noise(3)
-		fixed_noises.update(most_used_noise)
-		
-		json_dict[feature] = {
-			"accuracy":f"{accuracy:.2f}%",
-			"most_used_noise":most_used_noise
-		}
+    for noise in fixed_noises:
+        attacker = FixedNoiseAttacker(os.path.join(ROOT_PATH,f"{noise.split('.')[0]}_attack"))
+        accuracy = attacker.test_attack(noise)
 
-	for noise in fixed_noises:
-		attacker = FixedNoiseAttacker(os.path.join(ROOT_PATH,f"{noise.split('.')[0]}_attack"))
-		accuracy = attacker.test_attack(noise)
+        attacker.noise_maker.remove_generated_files()
 
-		attacker.noise_maker.remove_generated_files()
+        result_str = f"{noise} attack accuracy: {accuracy:.2f}\n"
+        print(result_str)
+        # log.write(result_str)
+        json_dict[noise] = {
+            "accuracy":f"{accuracy:.2f}%"
+        }
 
-		result_str = f"{noise} attack accuracy: {accuracy:.2f}\n"
-		print(result_str)
-		# log.write(result_str)
-		json_dict[noise] = {
-			"accuracy":f"{accuracy:.2f}%"
-		}
+    random_attack = RandomAttacker(os.path.join(ROOT_PATH,"random_attack"))
+    accuracy = random_attack.test_attack()
 
-	random_attack = RandomAttacker(os.path.join(ROOT_PATH,"random_attack"))
-	accuracy = random_attack.test_attack()
+    random_attack.noise_maker.remove_generated_files()
 
-	random_attack.noise_maker.remove_generated_files()
+    result_str = f"random attack accuracy: {accuracy:.2f}\n"
+    # print(result_str)
+    # log.write(result_str)
+    # log.close()
+    json_dict["random"] = {
+        "accuracy":f"{accuracy:.2f}%"
+    }
 
-	result_str = f"random attack accuracy: {accuracy:.2f}\n"
-	# print(result_str)
-	# log.write(result_str)
-	# log.close()
-	json_dict["random"] = {
-		"accuracy":f"{accuracy:.2f}%"
-	}
-
-	with open('noise_attack_log.json', 'w') as outfile:
-		json.dump(json_dict, outfile, indent=4)
+    with open('noise_attack_log.json', 'w') as outfile:
+        json.dump(json_dict, outfile, indent=4)
